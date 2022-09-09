ec3dcdee9cf82b4b814e0444bbbadf3a6344d175
natsat0919 <natalia.bajnok@gmail.com>
2022-09-09 02:45:04 +0200

    card texts

---
+++ mdi/card_texts.py
@@ -0,0 +1,36 @@
+# If no title assigned then title is a number
+# Deployment over time plot
+dot_title = "Deployments over time"
+dot_under_title = ""
+dot_info_circle = ""
+
+# Sunburst plot
+sbp_title = "Distribution between organisations and missions"
+sbp_under_title = ""
+sbp_info_circle = ""
+
+# Deployment per capita bar plot
+dpc_under_title = "of deployed troops per 100,000 capita on average"
+dpc_info_circle = ""
+
+# Deployment percentage of active troops bar plot
+dap_under_title = "of available active duty personnel on average"
+dap_info_circle = ""
+
+# Top deployment theatre meter plot
+tdm_under_title = "top deployment theatre"
+tdm_info_circle = ""
+
+# Total deployment personnel orgs breakdown per country bar plot
+tdop_title = "Total deployed personnel"
+tdop_under_title = "per country by different organisations"
+tdop_info_circle = ""
+
+# Total deployment bar plot
+tdp_under_title = "total troop deployment"
+tdp_info_circle = ""
+
+# MDI card
+mdi_under_title = "Millitary Deployments Index (MDI)"
+mdi_info_circle = ""
+mdi_extra_text = "TEXT ABOUT MDI"
