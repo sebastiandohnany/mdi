868e56186d534e3aee6cad5d6ec6a44fb57a930d
natsat0919 <natalia.bajnok@gmail.com>
2022-09-21 10:38:37 +0200

    id error suppress

--- mdi/server.py
+++ mdi/server.py
@@ -7,4 +7,5 @@ app = Dash(
     title="Military Deployments Index",
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
     external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME],
+    suppress_callback_exceptions=True,
 )
