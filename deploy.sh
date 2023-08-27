03fc4549472505ba471d8e4dceb83f8f84288d1a
sebastiandohnany <dohnany.sebastian@gmail.com>
2023-08-27 00:41:30 -0700

    deployment process that worked

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,13 @@ else
     exit 1
 fi
 
+read -p "Press Enter to install necessary packages..."
+ssh ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP} << EOF
+    sudo apt update
+    sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx
+    exit
+EOF
+
 # Create an app directory
 read -p "Press Enter to clone the git repo"
 ssh ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP} << EOF
@@ -25,18 +32,11 @@ EOF
 read -p "Press Enter to copy the Mapbox token file to the remote server..."
 scp ${MAPBOX_TOKEN_FILE} ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP}:${APP_DIRECTORY}/.mapbox_token
 
-Add an .env file with project root
+# Add an .env file with project root
 read -p "Press Enter to add .env file"
 echo "PROJECT_ROOT=${APP_DIRECTORY}/" > .env
 scp .env ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP}:${APP_DIRECTORY}/.env
 rm .env
-
-read -p "Press Enter to install necessary packages..."
-ssh ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP} << EOF
-    sudo apt update
-    sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx
-    exit
-EOF
 
 read -p "Press Enter to create venv and install dependencies..."
 ssh ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP} << EOF
@@ -52,9 +52,10 @@ read -p "Press Enter to configure and enable the WSGI server (Gunicorn)..."
 ssh ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP} << EOF
     source ${VENV_DIRECTORY}/bin/activate
     pip install gunicorn
-    sudo cp ${APP_DIRECTORY}/gunicorn.service /etc/systemd/system/
-    sudo systemctl start gunicorn
-    sudo systemctl enable gunicorn
+    deactivate
+    sudo cp ${APP_DIRECTORY}/mdi.service /etc/systemd/system/
+    sudo systemctl start mdi
+    sudo systemctl enable mdi
     exit
 EOF
 
@@ -66,13 +67,16 @@ ssh ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP} << EOF
 
     # Adjust firewall settings if needed (allow Nginx)
     sudo ufw allow 'Nginx Full'
+
+    # Sometimes access to directory needs to be allowed
+    sudo chmod 755 ${APP_DIRECTORY}
     exit
 EOF
 
 read -p "Press Enter to install ssl"
 ssh ${REMOTE_SERVER_USERNAME}@${REMOTE_SERVER_IP} << EOF
     sudo apt install -y certbot python3-certbot-nginx
-    sudo certbot --nginx --non-interactive --agree-tos -m ${EMAIL} -d ${DOMAIN}
+    sudo certbot --non-interactive --nginx --agree-tos -m ${EMAIL} -d ${DOMAIN} -d ${DOMAIN-WWW}
     exit
 EOF
 
